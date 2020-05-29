d9a2898d5972b4f2650481cedcfe6b0fa461d311
Nguyen Le Thinh <thinhnl@geniee.co.jp>
2020-05-29 16:15:37 +0700

    faqs

--- resources/views/content/category/faq.blade.php
+++ resources/views/content/category/faq.blade.php
@@ -9,129 +9,33 @@
 
     <div class="cont">
         <article class="main-column">
-
             <header class="article-header">
-              <h1> Can I book PACK45 workouts?</h1>
+                <h1>{{ $category->title }}</h1>
             </header>
 
             <div class="article-body markdown">
-              <p>Nope, you don't need to book PACK45 Workouts. Simply turn up 15 mins before it starts to guarantee your place.</p>
+                <ul class="faq__list">
+                    @foreach ($category->articles as $key => $article)
+                        <li class="faq__item">
+                            <div class="faq__title">{{ $article->title }}</div>
+                            <div class="faq__content">{!! $article->content !!}</p></div>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
-
-            <div class="article-attachments">
-              <ul class="attachments">
-                
-              </ul>
-            </div>
-
-            <footer class="article-footer">
-              
-                <div class="article-vote">
-                  <span class="article-vote-question">Was this article helpful?</span>
-                  <div class="article-vote-controls">
-                    <a class="article-vote-up" data-auth-action="signin" aria-selected="false" role="button" rel="nofollow" title="Yes" href="/hc/en-gb/signin?return_to=https%3A%2F%2Feasygym.zendesk.com%2Fhc%2Fen-gb%2Farticles%2F207938585-Can-I-book-PACK45-workouts-"></a>
-                    <a class="article-vote-down" data-auth-action="signin" aria-selected="false" role="button" rel="nofollow" title="No" href="/hc/en-gb/signin?return_to=https%3A%2F%2Feasygym.zendesk.com%2Fhc%2Fen-gb%2Farticles%2F207938585-Can-I-book-PACK45-workouts-"></a>
-                  </div>
-                  <small class="article-vote-count">
-                    <span class="article-vote-label">1 out of 1 found this helpful</span>
-                  </small>
-                </div>
-              
-              
-          <ul class="share">
-            <li><a href="https://www.facebook.com/share.php?title=Can+I+book+PACK45+workouts%3F&amp;u=https%3A%2F%2Feasygym.zendesk.com%2Fhc%2Fen-gb%2Farticles%2F207938585-Can-I-book-PACK45-workouts-" class="share-facebook">Facebook</a></li>
-            <li><a href="https://twitter.com/share?lang=en&amp;text=Can+I+book+PACK45+workouts%3F&amp;url=https%3A%2F%2Feasygym.zendesk.com%2Fhc%2Fen-gb%2Farticles%2F207938585-Can-I-book-PACK45-workouts-" class="share-twitter">Twitter</a></li>
-            <li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;source=easyGym&amp;title=Can+I+book+PACK45+workouts%3F&amp;url=https%3A%2F%2Feasygym.zendesk.com%2Fhc%2Fen-gb%2Farticles%2F207938585-Can-I-book-PACK45-workouts-" class="share-linkedin">LinkedIn</a></li>
-          </ul>
-
-
-            </footer>
-
-            <div class="article-more-questions">
-              Have more questions? <a href="/hc/en-gb/requests/new">Submit a request</a>
-            </div>
-
         </article>
 
         <aside class="article-sidebar side-column">
-            <section class="recent-articles"><h3 class="recent-articles-title">Recently viewed articles</h3><ul><li><a href="/hc/en-gb/articles/207938785-Are-there-showers-">Are there showers?</a></li><li><a href="/hc/en-gb/articles/203418232-What-are-the-opening-times-">What are the opening times?</a></li><li><a href="/hc/en-gb/articles/203417562-Is-there-parking-available-">Is there parking available?</a></li><li><a href="/hc/en-gb/articles/203750262-Can-I-use-chalk-including-liquid-chalk-in-the-gym-">Can I use chalk including liquid chalk in the gym?</a></li><li><a href="/hc/en-gb/articles/207938395-What-are-the-shower-facilities-like-">What are the shower facilities like?</a></li></ul></section>
-            
-          <section class="related-articles">
-            
-              <h3 class="related-articles-title">Related articles</h3>
-            
-            <ul>
-              
-                <li>
-                  <a href="/hc/en-gb/related/click?data=BAh7CjobZGVzdGluYXRpb25fYXJ0aWNsZV9pZGkES%2BRkDDoYcmVmZXJyZXJfYXJ0aWNsZV9pZGkEGeRkDDoLbG9jYWxlSSIKZW4tZ2IGOgZFVDoIdXJsSSI4L2hjL2VuLWdiL2FydGljbGVzLzIwNzkzODYzNS1Ib3ctbXVjaC1kb2VzLWl0LWNvc3QtBjsIVDoJcmFua2kG--937e1736b1c0d1212615ae62853aafbcdae4ad9b" rel="nofollow">How much does it cost?</a>
-                </li>
-              
-                <li>
-                  <a href="/hc/en-gb/related/click?data=BAh7CjobZGVzdGluYXRpb25fYXJ0aWNsZV9pZGkEHwZaDDoYcmVmZXJyZXJfYXJ0aWNsZV9pZGkEGeRkDDoLbG9jYWxlSSIKZW4tZ2IGOgZFVDoIdXJsSSI7L2hjL2VuLWdiL2FydGljbGVzLzIwNzIyNjM5OS1XaGVuLWFyZS1QQUNLNDUtd29ya291dHMtBjsIVDoJcmFua2kH--bd07f3e9895e702253f3a8c67ececed1bef017f2" rel="nofollow">When are PACK45 workouts?</a>
-                </li>
-              
-                <li>
-                  <a href="/hc/en-gb/related/click?data=BAh7CjobZGVzdGluYXRpb25fYXJ0aWNsZV9pZGkEW%2BNkDDoYcmVmZXJyZXJfYXJ0aWNsZV9pZGkEGeRkDDoLbG9jYWxlSSIKZW4tZ2IGOgZFVDoIdXJsSSJGL2hjL2VuLWdiL2FydGljbGVzLzIwNzkzODM5NS1XaGF0LWFyZS10aGUtc2hvd2VyLWZhY2lsaXRpZXMtbGlrZS0GOwhUOglyYW5raQg%3D--b3ea348b6b583be2870099f7447f2b6d31717405" rel="nofollow">What are the shower facilities like?</a>
-                </li>
-              
-                <li>
-                  <a href="/hc/en-gb/related/click?data=BAh7CjobZGVzdGluYXRpb25fYXJ0aWNsZV9pZGkEeOofDDoYcmVmZXJyZXJfYXJ0aWNsZV9pZGkEGeRkDDoLbG9jYWxlSSIKZW4tZ2IGOgZFVDoIdXJsSSI9L2hjL2VuLWdiL2FydGljbGVzLzIwMzQxODIzMi1XaGF0LWFyZS10aGUtb3BlbmluZy10aW1lcy0GOwhUOglyYW5raQk%3D--3d8e25d59133f08056087b254dec2baabbc788b1" rel="nofollow">What are the opening times?</a>
-                </li>
-              
-                <li>
-                  <a href="/hc/en-gb/related/click?data=BAh7CjobZGVzdGluYXRpb25fYXJ0aWNsZV9pZGkE4yYiDDoYcmVmZXJyZXJfYXJ0aWNsZV9pZGkEGeRkDDoLbG9jYWxlSSIKZW4tZ2IGOgZFVDoIdXJsSSJYL2hjL2VuLWdiL2FydGljbGVzLzIwMzU2NDc3MS1JbnRyb2R1Y3Rpb24tdG8tSElJVC1IaWdoLUludGVuc2l0eS1JbnRlcnZhbC1UcmFpbmluZy0GOwhUOglyYW5raQo%3D--731cbf623bb1115a16cccc4210551e34e79928aa" rel="nofollow">Introduction to HIIT (High Intensity Interval Training) </a>
-                </li>
-              
-            </ul>
-          </section>
-
-
+            <section class="faqs-category-list">
+                <h3 class="faq-section-title">Các chủ đề trợ giúp</h3>
+                <ul>
+                    @foreach ($categories as $key => $category)
+                        <li><a href="/category/{{ $category->slug }}">{{ $category->title }}</a></li>
+                    @endforeach
+                </ul>
+            </section>
           </aside>
-    </div>
-
-    <section class="site-section -blog-grey-bg">
-    <div class="blog-articles">
-
-        <div class="listQA">
-            @foreach($category->articles as $key => $article)
-                <a class="titleQA" href="/article/{{ $article->slug }}">
-                    <h3>{{ $article->title }}</h3>
-                </a>
-            @endforeach
-        </div>
-    </div>
-
-    <ul class="faq__list"><li class="faq__item">
-                    <div class="faq__title">What classes does it have?</div>
-                    <div class="faq__content">  <p>easyGym offers PACK45 – A TEAM-BASED workout experience, designed to get results. Whether you are a beginner, a weekend athlete or an experienced gym junkie, you can work at your pace, while lead by experienced level 3 certified PTs. You’ll also stay motivated and challenged as part of the ‘pack’ as you train together through a series of exercises that mix MMA, cardio, athletics and gymnastics. Burn fat, get conditioned, build strength. With consistent effort, you’ll feel results in 30 days, and, over time, you’ll transform yourself. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">IS THERE PARKING AVAILABLE?</div>
-                    <div class="faq__content">  <p>2 HOUR FREE PARKING AVAILABLE FOR ALL OUR MEMBERS in the Westgate Shopping Park. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">WHAT ARE THE PUBLIC TRANSPORT LINKS TO THE GYM?</div>
-                    <div class="faq__content">  <p>We are in Westgate Shopping Park just two minutes walk from Basildon Rail and Bus Stations. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">CAN I USE OTHER EASYGYMS</div>
-                    <div class="faq__content">  <p>AT THE MOMENT OUR SYSTEM DOES NOT ALLOW MULTI-SITE ACCESS, BUT WE ARE LOOKING AT OFFERING THIS SOON!</p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">CAN I BRING MY OWN PT?</div>
-                    <div class="faq__content">  <p>WE HAVE LICENSE AGREEMENTS WITH OUR OWN PERSONAL TRAINERS ON A SELF-EMPLOYED BASIS AND THEREFORE WE DO NOT ALLOW EXTERNAL PERSONAL TRAINERS TO TRAIN CLIENTS IN OUR GYM.</p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">Do you offer corporate memberships? </div>
-                    <div class="faq__content">  <p>The health and wellness of Basildon’s business community is important to us. If you would like to set up a corporate membership for your organisation, please email customerservices@easygym.co.uk to learn more. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">Do You partner with local businesses?</div>
-                    <div class="faq__content">  <p>Yes! We are proud to partner with local businesses throughout Essex and Basildon. Email us at customerservices@easygym.co.uk, and we’ll see what we can do!</p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">Where should we direct media enquiries? </div>
-                    <div class="faq__content">  <p>As Basildon’s local 24/7 gym, we are highly involved in supporting the health and wellness of our community. We are happy to participate in any local press, which promotes and inspires healthy choices. Contact customerservices@easygym.co.uk, and we’ll be happy to help. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">Do I have to have a gym induction?</div>
-                    <div class="faq__content">  <p>Inductions are not mandatory for new members. However, if you would like an induction, we can arrange one of our unique personalised inductions to help you feel comfortable starting an exercise program. </p></div>
-                </li><li class="faq__item">
-                    <div class="faq__title">What do you offer to help me get started?</div>
-                    <div class="faq__content">  <p>We offer unique personalised inductions, which are based around your individual needs and goals. Log-in to the easyGym member zone to book by appointment. </p></div>
-                </li></ul>
+    </div>    
 </section>
 @endsection
 
